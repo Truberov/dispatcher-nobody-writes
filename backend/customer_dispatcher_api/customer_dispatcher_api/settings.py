2b7868c07df77a119cae3d62ed327a37e2d4dfeb
Trubberov <90749206+Truberov@users.noreply.github.com>
2022-10-15 00:19:08 +0300

    add db, fix gitignore

--- backend/customer_dispatcher_api/customer_dispatcher_api/settings.py
+++ backend/customer_dispatcher_api/customer_dispatcher_api/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'customer_dispatcher_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "bd",
+        "USER": "admin",
+        "PASSWORD": "root",
+        "HOST": "10.0.24.252",
+        "PORT": "5436",
     }
 }
 
