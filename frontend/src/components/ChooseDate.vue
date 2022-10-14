2c868b400378d290304956dac27913fc61ba4209
wurfil <52235346+Wurfil@users.noreply.github.com>
2022-10-14 23:06:46 +0300

    start calendar

--- frontend/src/components/ChooseDate.vue
+++ frontend/src/components/ChooseDate.vue
@@ -2,6 +2,11 @@
   <v-row class="mt-10 justify-center">
     <h1>Выберете дату</h1>
   </v-row>
+  <v-row class="justify-center">
+    <v-col cols="8">
+
+    </v-col>
+  </v-row>
 </template>
 
 <script>
