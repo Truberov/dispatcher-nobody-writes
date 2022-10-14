2a85e8d266b0db0118b0e6132e3259cdf3db5bc4
wurfil <52235346+Wurfil@users.noreply.github.com>
2022-10-14 19:34:44 +0300

    fix

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
     <ToolBar/>
-    <CreateRequest/>
   </div>
 </template>
 
 <script>
 import ToolBar from '@/components/ToolBar.vue'
-import CreateRequest from '@/components/CreateRequest.vue'
 export default {
   name: 'HomeView',
   components: {
-    ToolBar,
-    CreateRequest
+    ToolBar
   }
 }
 </script>
